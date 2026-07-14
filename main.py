398d1f236e68584ab99d3b123d6bef1fd5bc9e46
Stiven Vallejo <stivjo96@gmail.com>
2026-07-13 21:34:31 -0500

    refactor: mejorar manejo de archivos con with open

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ ARCHIVO = "notas.txt"
 def registrar_estudiante(nombre, nota1, nota2, nota3):
     if nombre != "" and nota1 >= 0 and nota2 >= 0 and nota3 >= 0:
         promedio = (nota1 + nota2 + nota3) / 3
+
         if promedio >= 7:
             estado = "APROBADO"
         else:
             estado = "REPROBADO"
 
-        archivo = open(ARCHIVO, "a")
-        archivo.write(
-            nombre + "," +
-            str(nota1) + "," +
-            str(nota2) + "," +
-            str(nota3) + "," +
-            str(promedio) + "," +
-            estado + "\n"
-        )
-        archivo.close()
+        with open(ARCHIVO, "a") as archivo:
+            archivo.write(
+                nombre + "," +
+                str(nota1) + "," +
+                str(nota2) + "," +
+                str(nota3) + "," +
+                str(promedio) + "," +
+                estado + "\n"
+            )
+
         print("Registro guardado")
     else:
         print("Datos incorrectos")
@@ -27,38 +28,37 @@ def registrar_estudiante(nombre, nota1, nota2, nota3):
 
 def listar_registros():
     if os.path.exists(ARCHIVO):
-        f = open(ARCHIVO)
         print("-" * 70)
-        for linea in f:
-            datos = linea.strip().split(",")
-            print(
-                datos[0],
-                datos[1],
-                datos[2],
-                datos[3],
-                datos[4],
-                datos[5]
-            )
-        f.close()
+
+        with open(ARCHIVO, "r") as archivo:
+            for linea in archivo:
+                datos = linea.strip().split(",")
+
+                print(
+                    datos[0],
+                    datos[1],
+                    datos[2],
+                    datos[3],
+                    datos[4],
+                    datos[5]
+                )
     else:
         print("No existen registros")
 
 
 def mostrar_resumen():
     if os.path.exists(ARCHIVO):
-        f = open(ARCHIVO)
         aprobados = 0
         reprobados = 0
 
-        for x in f:
-            d = x.strip().split(",")
+        with open(ARCHIVO, "r") as archivo:
+            for linea in archivo:
+                datos = linea.strip().split(",")
 
-            if d[5] == "APROBADO":
-                aprobados += 1
-            else:
-                reprobados += 1
-
-        f.close()
+                if datos[5] == "APROBADO":
+                    aprobados += 1
+                else:
+                    reprobados += 1
 
         print("Aprobados:", aprobados)
         print("Reprobados:", reprobados)
